9e8c902265daa8e304f8de81dd73efb5636c697d
Eduardo Salazar <eduardosalaz@outlook.com>
2022-05-15 12:44:18 -0500

    tipos

---
+++ src/Types.jl
@@ -0,0 +1,37 @@
+module Types
+using JLD2, Plots, DelimitedFiles
+include("utils.jl")
+struct Instance
+    B::Int64
+    S::Int64
+    K::Int64
+    M::Int64
+    P::Int64
+    BU_coords::Matrix{Int64}
+    S_coords::Matrix{Int64}
+    D::Matrix{Int64}
+    Sk::Vector{Vector{Int64}}
+    Lk::Vector{Int64}
+    Uk::Vector{Int64}
+    V::Vector{Vector{Int64}}
+    μ::Vector{Vector{Int64}}
+    T::Vector{Float64}
+    R::Vector{Int64}
+    β::Vector{Int64}
+end
+
+struct Solution
+    Instance::Instance
+    X::Matrix{Int64}
+    Y::Vector{Int64}
+    Weight::Int64
+end
+
+export Instance, Solution
+export plot_instance, plot_solution
+export read_instance, write_instance
+export read_solution, write_solution
+export dump_solution
+
+
+end
---
+++ src/utils.jl
@@ -0,0 +1,154 @@
+function plot_instance(Instance, path::String)
+    Plots.scatter(
+        Instance.BU_coords[:, 1],
+        Instance.BU_coords[:, 2],
+        label = nothing,
+        markershape = :circle,
+        markercolor = :blue,
+    )
+    Plots.scatter!(
+        Instance.S_coords[:, 1],
+        Instance.S_coords[:, 2],
+        label = nothing,
+        markershape = :square,
+        markercolor = :white,
+        markersize = 6,
+        markerstrokecolor = :red,
+        markerstrokewidth = 2,
+    )
+    sizeB = Instance.B
+    sizeS = Instance.S
+    png(path)
+    @debug "Wrote plot and coords"
+end
+
+function plot_solution(Solution, path::String)
+    Instance = Solution.Instance
+    BU_coords = Instance.BU_coords
+    S_coords = Instance.S_coords
+    Y = Solution.Y
+    # Y = iszero.(Y)
+    X = Solution.X
+    S = Instance.S
+    B = Instance.B
+    Sk = Instance.Sk
+    S₁ = Sk[1]
+    S₁_coords = S_coords[S₁, :]
+    #Y₁_used = [(Y2[j] ? :red : :white) for j in S₁]
+    S₂ = Sk[2]
+    S₂_coords = S_coords[S₂, :]
+    #Y₂_used = [(Y2[j] ? :red : :white) for j in S₂]
+    S₃ = Sk[3]
+    S₃_coords = S_coords[S₃, :]
+    #Y₃_used = [(Y2[j] ? :red : :white) for j in S₃]
+    S₄ = Sk[4]
+    S₄_coords = S_coords[S₄, :]
+    #Y₄_used = [(Y2[j] ? :red : :white) for j in S₄]
+    S₅ = Sk[5]
+    S₅_coords = S_coords[S₅, :]
+   # Y₅_used = [(Y2[j] ? :red : :white) for j in S₅]
+    Plots.scatter(
+        BU_coords[:, 1],
+        BU_coords[:, 2],
+        markershape = :circle,
+        markercolor = :blue,
+        label = nothing
+    )
+    Plots.scatter!(
+        S₁_coords[:, 1],
+        S₁_coords[:, 2],
+        label = nothing,
+        markershape = :hexagon,
+        markercolor = :red,
+        markersize = 6,
+        markerstrokecolor = :red,
+        markerstrokewidth = 2,
+    )
+    Plots.scatter!(
+        S₂_coords[:, 1],
+        S₂_coords[:, 2],
+        label = nothing,
+        markershape = :diamond,
+        markercolor = :red,
+        markersize = 6,
+        markerstrokecolor = :red,
+        markerstrokewidth = 2,
+    )
+    Plots.scatter!(
+        S₃_coords[:, 1],
+        S₃_coords[:, 2],
+        label = nothing,
+        markershape = :star5,
+        markercolor = :red,
+        markersize = 6,
+        markerstrokecolor = :red,
+        markerstrokewidth = 2,
+    )
+    Plots.scatter!(
+        S₄_coords[:, 1],
+        S₄_coords[:, 2],
+        label = nothing,
+        markershape = :pentagon,
+        markercolor = :red,
+        markersize = 6,
+        markerstrokecolor = :red,
+        markerstrokewidth = 2,
+    )
+    Plots.scatter!(
+        S₅_coords[:, 1],
+        S₅_coords[:, 2],
+        label = nothing,
+        markershape = :star4,
+        markercolor = :red,
+        markersize = 6,
+        markerstrokecolor = :red,
+        markerstrokewidth = 2,
+    )
+
+    for i in 1:S
+        for j in 1:B
+            if X[i,j] == 1
+                Plots.plot!(
+                    [BU_coords[j,1], S_coords[i,1]],
+                    [BU_coords[j,2], S_coords[i,2]],
+                    color = :black,
+                    label = nothing,
+                )
+            end
+        end
+    end
+    png(path)
+    @debug "Wrote plot"
+
+end
+
+function read_instance(path::String)
+    instancia = jldopen(path)
+    instance = instancia["instance"]
+    return instance
+end
+
+function write_instance(instance, path::String)
+    jldsave(path; instance)
+end
+
+function read_solution(path::String)
+    sol = jldopen(path)
+    solution = sol["solution"]
+    return solution
+end
+
+function write_solution(solution, path::String)
+    jldsave(path; solution)
+end
+
+
+function dump_solution(path::String)
+    sol = jldopen(path)
+    solution = sol["solution"]
+    open("dump_solution.txt", "w") do io
+        writedlm(io, solution.X)
+        writedlm(io, solution.Y)
+        writedlm(io, solution.Weight)
+    end
+end
