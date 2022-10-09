61354f39482aab14ef97ae4a3a401b347cce91ab
Eduardo Salazar <eduardosalaz@outlook.com>
2022-10-09 16:59:58 -0500

    solo dpi, no size

--- src/utils.jl
+++ src/utils.jl
@@ -26,7 +26,7 @@ function plot_instance(Instance, path::String)
         markershape = :circle,
         markercolor = :blue,
         label = "BUs",
-        size = (1200, 800),
+        dpi = 500
     )
     Plots.scatter!(
         S₁_coords[:, 1],
